63add7c59513fcd1dc9f9b67ef0ad73a1c641fe6
HirosuguTakeshita <takeshita.hirosugu.17@shizuoka.ac.jp>
2020-01-18 03:44:30 +0900

    Significant refactoring

--- YouAdSkipper.js
+++ YouAdSkipper.js
@@ -1,20 +1,12 @@
-const host_url = location.host;
-
-if (host_url === "www.youtube.com") {
-    setInterval(
-        function () {
-            try {
-                const elem1 = document.getElementsByClassName("ytp-ad-overlay-close-container");
-                elem1[0].click();
-            } catch (e) {
-                ;
-            }
-            try {
-                const elem2 = document.getElementsByClassName("ytp-ad-skip-button-container");
-                elem2[0].click();
-            } catch (e) {
-                ;
-            }
-        }, 1000
-    )
+const doSkipAndClose = () => {
+    const close_button = document.getElementsByClassName("ytp-ad-overlay-close-container");
+    const skip_button = document.getElementsByClassName("ytp-ad-skip-button-container");
+    if (close_button) {
+        close_button[0].click();
+    }
+    if (skip_button) {
+        skip_button[0].click();
+    }
 }
+
+setInterval(doSkipAndClose(), 1000);
